66eebfba5e5504f05b17ee40375402bbaf6e8332
sabinevi <sabinevi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-18 21:36:44 +0000

    Added thickbox functionality
    git-svn-id: https://plugins.svn.wordpress.org/sabines-zoom-gallery/tags/0.3@742927 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,78 @@
+=== Sabines Zoom Gallery ===
+Contributors: sabinevi
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SGZZFEAK5YSEE
+Tags: image, photo, gallery, inner zoom, captions, javascript, jquery
+Requires at least: 3.5.0
+Tested up to: 3.5.2
+Stable Tag: 0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Creates a gallery of all the attached images to a page or post, with an inner zoom to the full image.
+
+== Description ==
+
+Have a nice inner zoom on your gallery images. It shows only the images, no clickable thumbs, and shows an inner zoom onmouseover. The zoom effect depends heavily on the resolution of the image you have uploaded; a bigger original gives a better zoom effect.
+
+There are two configuration options to add to the shortcode: 
+1. the parameter 'showsize' and the desired size (thumbnail, medium or large): `[sabineszoom showsize=medium]`
+2. the parameter 'thickbox' with the value 1: `[sabineszoom thickbox=1]`
+or both: `[sabineszoom showsize=medium thickbox=1]`
+
+If you want to use this gallery, simply use the [sabineszoom] shortcode in your post or page, or add `<?php echo do_shortcode('[sabineszoom]'); ?>` within the loop, to your template.
+By default the 'large' image is used, if you want the 'medium' or 'thumbnail' to show, add the parameter 'showsize' like so: `[sabineszoom showsize=medium]`. Valid options are **thumbnail**, **medium** and **large**.
+
+This is my first plugin, originally build for a customer who couldn't find a gallery plugin that fitted his needs exactly:
+no gallery of thumbnails that should be clicked on, this is just a list of the images attached to a post or page that have an inner zoom to the full image.
+
+I used the jQuery plugin Image Zoom by elevate zoom for the inner zoom effect (see http://www.elevateweb.co.uk/image-zoom/examples#inner-zoom) .
+
+Please let me know if you need help or have a feature request.
+
+== Installation ==
+
+1. Unzip and activate
+2. Set the desired size for your images on the Settings > Media page **BEFORE** uploading your images to your post or page
+3. Open the editor of the post or page where you want the gallery to appear
+4. Upload your picture(s) by clicking the **Add media**-button just above the buttonbar. **DON'T USE** the **Insert into post** button, the images are automatically connected to the post. The zoom effect depends heavily on the resolution of the image you upload; a bigger original gives a better zoom effect.
+5. Add the shortcode `[sabineszoom]` to your post or page
+6. By default the large image is used, use `[sabineszoom showsize=medium]` if you want the **medium** size or `[sabineszoom showsize=thumbnail]` for the **thumbnail** 
+7. Publish the post or page.
+
+== Frequently Asked Questions ==
+
+= There's no or minor inner zoom effect on my images =
+Check the dimensions of the original image and the dimensions set in Settings > Media. Your original image should be larger than the dimensions set for the large (or your preferred size) image in the Media settingspage.
+
+= How about captions? =
+If you have a text in the captions area of your image in the Media Library, it is shown on the picture. The text disappears on mouseover. If you don't want the captions to be shown but don't want to delete them, use a display: none; in your stylesheet. 
+You can change the style of the captions by overriding the css in your own stylesheet.
+
+= How do I use the medium or thumbnail size image instead of the large image in the gallery view? =
+Use `[sabineszoom showsize=medium]` if you want the **medium** size or `[sabineszoom showsize=thumbnail]` for the **thumbnail**
+
+== Screenshots ==
+
+1. Upload the pictures in which you want the zoom effect when you are editing your page or post. That way they get attached to the post or page. Add the shortcode [sabineszoom] to the text.
+2. View the images already uploaded to the current page or post by selecting **Uploaded to this page** option from the pulldown menu. **Don't insert the images into the textfield.**
+3. By default the large image is shown
+4. The caption disappears onmousever
+
+== Changelog ==
+
+= 0.3 =
+* Added Thickbox option
+
+= 0.2.1 =
+* Renamed functions by adding a prefix to prevent confusion
+
+= 0.2 =
+* Added 'showsize' attribute to shortcode
+* Enhanced script- and style enqueing
+
+= 0.1 =
+* First version April 2013
+
+== Upgrade Notice ==
+
+N.a.  
---
+++ sabines-zoom-gallery.php
@@ -0,0 +1,145 @@
+<?php
+/*
+Plugin Name: Sabines Zoom Gallery
+Plugin URI: http://www.sabine.nl/innerzooom-test/
+Description: Gallery plugin to make the large pic to zoom into the full version, no thumbnails
+Version: 0.3
+Author: Sabine Visser
+Author URI: http://www.sabine.nl
+License: GPL2
+*/
+
+/*
+Sabines Zoom Gallery (Wordpress Plugin)
+Copyright (C) 2013 Sabine Visser
+Contact me at http://www.sabine.nl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$sabineZoomGallery = new SabinesZoomGallery();
+
+class SabinesZoomGallery {
+    
+    protected $showLightBox;
+    
+    /**
+     * register shortcode
+     * load zoom javasscript
+     * load zoom stylesheet 
+     */
+    function __construct() {
+		
+        // setup the gallery, this might update the value of $this->showLightBox;
+        add_shortcode('sabineszoom', array($this, 'sv_generate_gallery'));
+
+        // @TODO move this somewhere else where it's not loaded if there's no gallery
+        add_action('wp_head', array(&$this,'sv_add_js_zoom_init'));
+
+        add_action( 'wp_enqueue_scripts', array(&$this,'sv_add_styles_and_scripts') );
+    }
+
+
+    /**
+    * @param $attr array with user input in shortcode
+    * @return html-string
+    **/
+    function sv_generate_gallery($attr) {
+
+        global $post;
+        $gallery        = '';
+        $sizeToShow     = 'large'; // defaults to the large image
+        $aPossibleSizes = array('thumbnail','medium','large');
+        $this->showLightBox = false;
+        
+        // extract attributes if any
+        if(isset($attr['showsize']) && in_array($attr['showsize'],$aPossibleSizes,true)) {
+            $sizeToShow = $attr['showsize'];
+        }
+        if(isset($attr['thickbox'])) {
+            $this->showLightBox = true;
+        }
+
+        // get all attachments of the current post
+        $args = array(
+                'post_type' => 'attachment',
+                'numberposts' => -1,
+                'post_status' => null,
+                'post_parent' => $post->ID,
+                'post_mime_type' => 'image',
+                'post_status' => 'inherit'
+                );
+
+        $attachments = get_posts( $args );
+        
+        if ( $attachments ) {
+            
+            foreach ( $attachments as $attachment ) {
+                $aImgLarge	= wp_get_attachment_image_src( $attachment->ID, $sizeToShow );
+                $aImgFull	= wp_get_attachment_image_src( $attachment->ID, 'full' );
+
+                $gallery .= '<div class="zoomer-wrapper">'."\n\t";
+                // check for lightbox
+                if( $this->showLightBox ) {
+                    $gallery .= '<a href="'.$aImgFull[0].'" rel="tb-gallery" class="thickbox"><img class="zoomer" src="'.$aImgLarge[0].'" data-zoom-image="'.$aImgFull[0].'" /></a>'."\n\t";
+                } 
+                else {
+                    $gallery .= '<img class="zoomer" src="'.$aImgLarge[0].'" data-zoom-image="'.$aImgFull[0].'" />'."\n\t";
+                }
+				// caption
+                if( !empty($attachment->post_excerpt) ) {
+                    $gallery .= '<span class="zoomer-caption">'.$attachment->post_excerpt.'</span>'."\n";
+                }
+                $gallery .= '</div>'."\n";
+                
+            }
+            
+        }
+        return $gallery;
+    }
+    
+    
+    /**
+     * new way to add javascripts and stylesheets to the queue
+     */
+    function sv_add_styles_and_scripts() {
+
+        wp_enqueue_style('sabineszoomgallerystylesheet', WP_PLUGIN_URL . '/sabines-zoom-gallery/css/sabines-style.css',false); 
+
+        wp_enqueue_script('jquery');
+
+        wp_enqueue_script('sabineszoomgalleryscript', WP_PLUGIN_URL . '/sabines-zoom-gallery/js/jquery.elevateZoom-2.5.5.min.js', false);
+		
+		// @TODO find a way to not add these two if thickbox is not desired
+		wp_enqueue_script('thickbox', null,  array('jquery'));
+		
+		wp_enqueue_style('thickbox.css', '/'.WPINC.'/js/thickbox/thickbox.css', null, '1.0');
+		
+    }
+
+	   
+    /**
+    * add initscript to head section
+    **/
+    function sv_add_js_zoom_init() {
+		
+        echo '<script>
+                jQuery(window).load(function(){
+                    jQuery(".zoomer").elevateZoom({ zoomType: "inner", cursor: "crosshair"});
+                });
+            </script>';
+		echo '<style>#TB_window { z-index: 999; }</style>';
+    }
+}
+
